6d4ad12576de4eafe2bae8b1e7af0b0b4824b262
Danman05 <danielspurrell@hotmail.com>
2024-02-13 13:26:46 +0100

    SignalRCsharpTestingClient

---
+++ Debugger Tools/SignalR Debug Client/Lightweight SignalR Client for backend connectivity testing/ChatClient.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Lightweight_SignalR_Client_for_backend_connectivity_testing
+{
+        /*
+        * Refactored code from Program.cs
+        * 
+        */
+    public class ChatClient
+    {
+        private readonly string _username; // Client's username
+        private readonly HubConnection _connection; // Hub Connection
+
+        // Connection strings
+        private static readonly string _localConnection = "https://localhost:7206/chatHub";
+        private readonly string _connectionString = "http://192.168.1.246:8080/chatHub";
+
+        /// <summary>
+        /// Builds a connections from the HubConnectionBuilder
+        /// </summary>
+        /// <param name="username">passed as query parameter</param>
+        public ChatClient(string username)
+        {
+            _username = username;
+            _connection = new HubConnectionBuilder()
+                .WithUrl($"{_localConnection}?username={_username}")
+                .Build();
+        }
+
+
+        // Start connection and add listeners
+        public async Task StartAsync()
+        {
+            _connection.On<string, string>("MessageReceived", (user, message) =>
+            {
+                Console.WriteLine($"Received message from {user}: {message}");
+            });
+
+            _connection.On<string>("error", (err) =>
+            {
+                Console.WriteLine($"error: {err}");
+                throw new Exception(err);
+            });
+
+            _connection.On<string>("adduser", (user) =>
+            {
+                Console.WriteLine($"{user} has joined the chat");
+            });
+
+            await _connection.StartAsync();
+        }
+
+        /// <summary>
+        /// Disposes connection
+        /// </summary>
+        /// <returns></returns>
+        public async Task DisposeAsync()
+        {
+            await _connection.DisposeAsync();
+        }
+
+        /// <summary>
+        /// Sends a message to server
+        /// </summary>
+        /// <param name="message">Message content</param>
+        /// <returns></returns>
+        public async Task SendMessageAsync(string message)
+        {
+            await _connection.SendAsync("SendMessage", _username, message);
+        }
+
+        /// <summary>
+        /// Retrieve active user list
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<string>> RequestUserListAsync()
+        {
+            return await _connection.InvokeAsync<List<string>>("RequestUserList", _username);
+        }
+    }
+}
---
+++ Debugger Tools/SignalR Debug Client/SignalRClient.Tests/SignalRClientTest.cs
@@ -0,0 +1,46 @@
+﻿using Lightweight_SignalR_Client_for_backend_connectivity_testing;
+using Xunit;
+
+namespace SignalRClient.Tests
+{
+    public class SignalRClientTest
+    {
+        [Theory]
+        [InlineData("Hello world")]
+        [InlineData("This message contains profanity")]
+        [InlineData("This message contains issues")]
+        [InlineData("This message contains commands such as /kick william")]
+
+        public async Task SendMessageAsync_ShouldSendMessage(string message)
+        {
+            // Arrange
+            var chatClient = new ChatClient("CSharp");
+            // Connect
+            await chatClient.StartAsync();
+            // Assert
+            await chatClient.SendMessageAsync(message);
+
+            // End
+            await chatClient.DisposeAsync();
+        }
+
+        [Fact]
+        public async Task RequestUserListAsync_ShouldRetrieveUserList()
+        {
+
+            // Arrange
+            var chatClient = new ChatClient("C-Sharp lightweightclient");
+
+            await chatClient.StartAsync();
+
+            // Act
+            List<string> results = await chatClient.RequestUserListAsync();
+
+            // Assert
+            Assert.NotNull(results);
+
+            // End
+            await chatClient.DisposeAsync();
+        }
+    }
+}
